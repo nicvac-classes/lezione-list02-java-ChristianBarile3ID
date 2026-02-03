4ca577d88fee890623a5d5be5b6117f1e4a56a52
ChristianBarile3ID <christianalessandro.barile@marconibari.edu.it>
2026-02-03 11:32:11 +0000

    03-02

--- Lista.java
+++ Lista.java
@@ -1,1 +1,34 @@
-//Implementa qui la classe Lista
+public class Lista<T> {
+
+    Nodo<T> head;
+    
+    public Lista<T>(Nodo<T> head) {
+        head = null;
+    }
+
+    public boolean vuota() {
+        if (head.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void aggiungiInTesta(T dato) {
+        Nodo<T> nuovoNodo = new Nodo(dato);
+        nuovoNodo.next = head;
+        head = nuovoNodo;
+    }
+
+    public void aggiungiInCoda(T dato) {
+        Nodo <T> nuovoNodo = new Nodo(dato);
+
+        if (head == null) {
+            nuovoNodo.next = null;
+            head = nuovoNodo;
+        }
+        
+    }
+
+
+}
