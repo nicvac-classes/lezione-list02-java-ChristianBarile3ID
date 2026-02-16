14d885436d10aff46b898db8f35fba567867f866
ChristianBarile3ID <christianalessandro.barile@marconibari.edu.it>
2026-02-16 13:32:41 +0000

    Consegna

--- Lista.java
+++ Lista.java
@@ -24,11 +24,26 @@ public class Lista<T> {
         Nodo <T> nuovoNodo = new Nodo(dato);
 
         if (head == null) {
-            nuovoNodo.next = null;
             head = nuovoNodo;
         }
-        
+
+        Nodo<T> curr = head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = nuovoNodo;
     }
 
+    public void aggiungiInPosizione(T dato, int posizione) {
+        Nodo <T> nuovoNodo = new Nodo<>(dato);
+        Nodo<T> curr = head;
+        currI = 0;
+        while (currI<posizione-1) {
+            curr = curr.next;
+            ++currI;
+        }
+        nuovoNodo.next = curr.next;
+        curr.next = nuovoNodo;
+    }
 
 }
